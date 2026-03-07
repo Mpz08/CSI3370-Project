961183c2371695daff2df9de196f00a23a9ed93d
MinhPhan20 <rooneyphan@yahoo.com>
2026-03-07 12:26:45 -0500

    Initial commit: Added Home screen and Guild Search UI

---
+++ app/src/main/java/com/example/platemate/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.platemate;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        // Find the button and make it open the new screen
+        android.widget.Button btnFind = findViewById(R.id.btnFindGuide);
+        btnFind.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                android.content.Intent intent = new android.content.Intent(MainActivity.this, GuideListActivity.class);
+                startActivity(intent);
+            }
+        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
