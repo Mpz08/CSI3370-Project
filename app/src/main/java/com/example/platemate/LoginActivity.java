47f7d90c5ac379f742e72e531adfe26f4db3f7b9
anthony nannoshi <anthonynannoshi@oakland.edu>
2026-04-11 23:20:31 -0400

    saving current work

--- app/src/main/java/com/example/platemate/LoginActivity.java
+++ app/src/main/java/com/example/platemate/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.example.platemate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.CSI3370.recipedock.R;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,25 +16,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        // Find the register/login button by its ID
-        android.widget.Button btnLogin = findViewById(R.id.btnRegister);
 
-        // Tell the button what to do when clicked
-        btnLogin.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                // Create an Intent to go to the Home Screen (MainActivity)
-                android.content.Intent intent = new android.content.Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+        Button btnLogin = findViewById(R.id.btnRegister);
 
-                // This closes the login screen so the user can't press the "back" button to return to it
-                finish();
-            }
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, TrendingActivity.class);
+            startActivity(intent);
+            finish();
         });
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
     }
 }
