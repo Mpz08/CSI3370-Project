6052f0098e93794df50021275dad839b5b7be6b4
Admin <rooneyphan@yahoo.com>
2026-04-12 09:44:26 +0700

    Added functional Go Back buttons to all screens

--- app/src/main/java/com/example/platemate/GuideListActivity.java
+++ app/src/main/java/com/example/platemate/GuideListActivity.java
@@ -4,21 +4,26 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.graphics.Insets;
-//import androidx.core.view.ViewCompat;
-//import androidx.core.view.WindowInsetsCompat;
 
 public class GuideListActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_guide_list);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+
+        // 1. Find the back button we just added to the XML
+        android.widget.Button btnGoBack = findViewById(R.id.btnGoBack);
+
+        // 2. Tell the button to listen for a click
+        btnGoBack.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                // 3. Close this Search screen and drop back to the Main screen!
+                finish();
+            }
+        });
+
     }
+
 }
